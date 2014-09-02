7e55cd3f007c364abadb3c31d4fe5dc793bb622c
Joseph Bernstein <joe@bountyapp.co>
2014-09-02 16:30:39 -0700

    stdout

--- log.go
+++ log.go
@@ -1,43 +1,43 @@
 package log
 
 import (
-	"log"
-	"io/ioutil"
-	"os"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var trace = log.New(ioutil.Discard, "T: ", log.Ldate|log.Ltime|log.Lshortfile)
-var info = log.New(os.Stderr, "I: ", log.Ldate|log.Ltime|log.Lshortfile)
-var warning = log.New(os.Stderr, "W: ", log.Ldate|log.Ltime|log.Lshortfile)
-var error = log.New(os.Stderr, "E: ", log.Ldate|log.Ltime|log.Lshortfile)
-var slack = log.New(os.Stderr, "SLACK: ", log.Ldate|log.Ltime|log.Lshortfile)
-var request = log.New(os.Stderr, "R: ", log.Ldate|log.Ltime)
+var info = log.New(os.Stdout, "I: ", log.Ldate|log.Ltime|log.Lshortfile)
+var warning = log.New(os.Stdout, "W: ", log.Ldate|log.Ltime|log.Lshortfile)
+var error = log.New(os.Stdout, "E: ", log.Ldate|log.Ltime|log.Lshortfile)
+var slack = log.New(os.Stdout, "SLACK: ", log.Ldate|log.Ltime|log.Lshortfile)
+var request = log.New(os.Stdout, "R: ", log.Ldate|log.Ltime)
 
 var callDepth int = 2
 
 func Errorf(format string, v ...interface{}) {
-	error.Output(callDepth,f(format, v...))
+	error.Output(callDepth, f(format, v...))
 }
 
 func Errorln(v ...interface{}) {
-	error.Output(callDepth,ln(v...))
+	error.Output(callDepth, ln(v...))
 }
 
 func Warningf(format string, v ...interface{}) {
-	warning.Output(callDepth,f(format, v...))
+	warning.Output(callDepth, f(format, v...))
 }
 
 func Warningln(v ...interface{}) {
-	warning.Output(callDepth,ln(v...))
+	warning.Output(callDepth, ln(v...))
 }
 
 func Infof(format string, v ...interface{}) {
-	info.Output(callDepth,f(format, v...))
+	info.Output(callDepth, f(format, v...))
 }
 
 func Infoln(v ...interface{}) {
-	info.Output(callDepth,ln(v...))
+	info.Output(callDepth, ln(v...))
 }
 
 func Requestln(v ...interface{}) {
@@ -45,11 +45,11 @@ func Requestln(v ...interface{}) {
 }
 
 func Tracef(format string, v ...interface{}) {
-	trace.Output(callDepth,f(format, v...))
+	trace.Output(callDepth, f(format, v...))
 }
 
 func Traceln(v ...interface{}) {
-	trace.Output(callDepth,ln(v...))
+	trace.Output(callDepth, ln(v...))
 }
 
 func Panicln(v ...interface{}) {
@@ -59,17 +59,17 @@ func Panicln(v ...interface{}) {
 }
 
 func Panicf(format string, v ...interface{}) {
-	string := f(format,v...)
+	string := f(format, v...)
 	trace.Output(callDepth, string)
 	panic(string)
 }
 
 func Slackf(format string, v ...interface{}) {
-	slack.Output(callDepth,f(format, v...))
+	slack.Output(callDepth, f(format, v...))
 }
 
 func SlackLn(v ...interface{}) {
-	slack.Output(callDepth,ln(v...))
+	slack.Output(callDepth, ln(v...))
 }
 
 func f(format string, v ...interface{}) string {
