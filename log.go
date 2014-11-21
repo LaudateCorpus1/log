b2bb38a1946b0eb2cb0c0ce4eb7c98467bf84af7
Joseph Bernstein <joe@bountyapp.co>
2014-11-21 13:50:47 -0800

    allow setting of prefix

--- log.go
+++ log.go
@@ -2,74 +2,154 @@ package log
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
-var trace = log.New(ioutil.Discard, "T: ", log.Ldate|log.Ltime|log.Lshortfile)
-var info = log.New(os.Stdout, "I: ", log.Ldate|log.Ltime|log.Lshortfile)
-var warning = log.New(os.Stdout, "W: ", log.Ldate|log.Ltime|log.Lshortfile)
-var error = log.New(os.Stderr, "E: ", log.Ldate|log.Ltime|log.Lshortfile)
-var slack = log.New(os.Stdout, "SLACK: ", log.Ldate|log.Ltime|log.Lshortfile)
-var request = log.New(os.Stdout, "R: ", log.Ldate|log.Ltime)
+type Logger struct {
+	info    *log.Logger
+	warning *log.Logger
+	error   *log.Logger
+	slack   *log.Logger
+	request *log.Logger
+}
+
+func New(prefix string) *Logger {
+
+	l := &Logger{
+		info:    log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile),
+		warning: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile),
+		error:   log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile),
+		slack:   log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile),
+		request: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+	}
+
+	l.SetPrefix(prefix)
+	return l
+}
+
+var DefaultLogger = New("")
+
+type BLogger interface {
+	Errorf(format string, v ...interface{})
+	Errorln(v ...interface{})
+	Warningf(format string, v ...interface{})
+	Warningln(v ...interface{})
+	Infof(format string, v ...interface{})
+	Infoln(v ...interface{})
+	Requestln(v ...interface{})
+	Panicln(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Slackf(format string, v ...interface{})
+	SlackLn(v ...interface{})
+}
 
 var callDepth int = 2
 
+func (this *Logger) SetPrefix(prefix string) {
+
+	if prefix != "" {
+		prefix = prefix + " "
+	}
+
+	this.info.SetPrefix(prefix + "I: ")
+	this.warning.SetPrefix(prefix + "W: ")
+	this.error.SetPrefix(prefix + "E: ")
+	this.slack.SetPrefix(prefix + "SLACK: ")
+	this.request.SetPrefix(prefix + "R: ")
+}
+
+func (this *Logger) Errorf(format string, v ...interface{}) {
+	this.error.Output(callDepth, f(format, v...))
+}
+
+func (this *Logger) Errorln(v ...interface{}) {
+	this.error.Output(callDepth, ln(v...))
+}
+
+func (this *Logger) Warningf(format string, v ...interface{}) {
+	this.warning.Output(callDepth, f(format, v...))
+}
+
+func (this *Logger) Warningln(v ...interface{}) {
+	this.warning.Output(callDepth, ln(v...))
+}
+
+func (this *Logger) Infof(format string, v ...interface{}) {
+	this.info.Output(callDepth, f(format, v...))
+}
+
+func (this *Logger) Infoln(v ...interface{}) {
+	this.info.Output(callDepth, ln(v...))
+}
+
+func (this *Logger) Requestln(v ...interface{}) {
+	this.request.Println(v...)
+}
+
+func (this *Logger) Panicln(v ...interface{}) {
+	string := ln(v...)
+	this.error.Output(callDepth, string)
+	panic(string)
+}
+
+func (this *Logger) Panicf(format string, v ...interface{}) {
+	string := f(format, v...)
+	this.error.Output(callDepth, string)
+	panic(string)
+}
+
+func (this *Logger) Slackf(format string, v ...interface{}) {
+	this.slack.Output(callDepth, f(format, v...))
+}
+
+func (this *Logger) SlackLn(v ...interface{}) {
+	this.slack.Output(callDepth, ln(v...))
+}
+
+//Globals
 func Errorf(format string, v ...interface{}) {
-	error.Output(callDepth, f(format, v...))
+	DefaultLogger.Errorf(format, v...)
 }
 
 func Errorln(v ...interface{}) {
-	error.Output(callDepth, ln(v...))
+	DefaultLogger.Errorln(v...)
 }
 
 func Warningf(format string, v ...interface{}) {
-	warning.Output(callDepth, f(format, v...))
+	DefaultLogger.Warningf(format, v...)
 }
 
 func Warningln(v ...interface{}) {
-	warning.Output(callDepth, ln(v...))
+	DefaultLogger.Warningln(v...)
 }
 
 func Infof(format string, v ...interface{}) {
-	info.Output(callDepth, f(format, v...))
+	DefaultLogger.Infof(format, v...)
 }
 
 func Infoln(v ...interface{}) {
-	info.Output(callDepth, ln(v...))
+	DefaultLogger.Infoln(v...)
 }
 
 func Requestln(v ...interface{}) {
-	request.Println(v...)
-}
-
-func Tracef(format string, v ...interface{}) {
-	trace.Output(callDepth, f(format, v...))
-}
-
-func Traceln(v ...interface{}) {
-	trace.Output(callDepth, ln(v...))
+	DefaultLogger.Requestln(v...)
 }
 
 func Panicln(v ...interface{}) {
-	string := ln(v...)
-	trace.Output(callDepth, string)
-	panic(string)
+	DefaultLogger.Panicln(v...)
 }
 
 func Panicf(format string, v ...interface{}) {
-	string := f(format, v...)
-	trace.Output(callDepth, string)
-	panic(string)
+	DefaultLogger.Panicf(format, v...)
 }
 
 func Slackf(format string, v ...interface{}) {
-	slack.Output(callDepth, f(format, v...))
+	DefaultLogger.Slackf(format, v...)
 }
 
 func SlackLn(v ...interface{}) {
-	slack.Output(callDepth, ln(v...))
+	DefaultLogger.SlackLn(v...)
 }
 
 func f(format string, v ...interface{}) string {
